828edbdead07e2697adef12284d2dbd0e643483a
vivesing <vivek.singh1693@gmail.com>
2025-01-30 22:48:10 +0530

    [desing-pattern]Adding changes for decorator client

--- designpattern/src/main/java/com/viv/structuraldesignpattern/decorator/Client.java
+++ designpattern/src/main/java/com/viv/structuraldesignpattern/decorator/Client.java
@@ -3,7 +3,7 @@ package com.viv.structuraldesignpattern.decorator;
 /**
  * Classes in Java I/O package are great example of decorator pattern
  * For example the java.io.BufferedReader class decorated any java.io.Outputstream objects adds buffering
- * to file writing operations.
+ * to file writing operations. This improves the disk i/o performace by reducing number of writes
  */
 public class Client {
 
